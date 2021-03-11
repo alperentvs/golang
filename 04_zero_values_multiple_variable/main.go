77932fb5878ca068705546429d46b640b971ff04
Alperen Tavas <alperenntvs@gmail.com>
2021-03-11 23:42:28 +0300

    04_zero_values_multiple_variable eklendi.

---
+++ 04_zero_values_multiple_variable/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/* var (
+		 name string = "Alperen"
+		 age int = 24
+		 isMarried bool = true
+
+		 weight float32 = 77.5
+		 height int = 190
+	) */
+
+	/* var name, age, isMarried, weight, height = "Alperen", 24, true, 77.5, 190 */
+
+	/* name, age, isMarried, weight, height := "Alperen", 24, true, 77.5, 190 */
+
+	var isMarried bool
+
+	fmt.Println(isMarried) // String --> "", numeric --> 0, bool --> false
+
+	/* 	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+	fmt.Println(weight)
+	fmt.Println(height) */
+
+}
