21b1a1641e3ca4d5b38fa8ba246b65f0b09a11f1
Alperen Tavas <alperenntvs@gmail.com>
2021-03-14 22:29:56 +0300

    08_naming_print eklendi.

---
+++ 08_naming_print/main.go
@@ -0,0 +1,60 @@
+package main
+
+/* var x = 10
+
+var Y = 100 */
+
+func main() {
+
+	// (Print - Println) - Printf
+
+	/* 	fmt.Println("Merhaba")
+	   	fmt.Print("Merhaba")
+	   	fmt.Println("")
+	   	fmt.Printf("Merhaba")	 */
+
+	/* name := "Arin" */
+
+	/* 	fmt.Print(name)
+	   	fmt.Println(name)
+	   	fmt.Printf(name) */
+
+	/* 	fmt.Print("Benim Adım", name)
+	   	fmt.Println("")
+	   	fmt.Println("Benim Adım ", name)
+	   	fmt.Printf("Benim Adım %X %T", name, name)
+
+		// the value in a default format
+		// %T	a Go-syntax representation of the type of the value */
+
+	/* 	x := 100
+	   	y := 20
+	   	z := 30
+
+	   	fmt.Printf("%b %d %o", x, y, z) */
+
+	// %b	base 2
+	// %d	base 10
+	// %o	base 8
+
+	/* name, age := "Alperen", 23 */
+
+	// fmt.Print("Benim Adım ", name, ", ve ben ", age, " yaşındayım.")
+	// fmt.Println("Benim Adım", name, "ve ben", age, "yaşındayım.")
+	// fmt.Printf("Benim Adım %v, ve ben %v yaşındayım", name, age)
+
+	//VISIBILITY
+
+	/* 	fmt.Println(x)
+
+	   	myFunc() */
+
+	/* 	var coin string // count - customer - coin
+	   	// Go camel case isimlendirme kullanılır.
+	   	var coinType string
+	   	var custName string
+	   	// kısaltmalar büyük harflerle yazılır
+	   	var URL // Url değil
+	   	var HTTP // http değil "xyzHTTP" */
+
+}
