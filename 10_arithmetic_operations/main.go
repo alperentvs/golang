40ae801aa3c56aa1edafae4bc32d0c19c32b2247
Alperen Tavas <alperenntvs@gmail.com>
2021-03-15 02:05:14 +0300

    10_arithmetic_operations eklendi.

---
+++ 10_arithmetic_operations/main.go
@@ -0,0 +1,37 @@
+// addition + => 15 + 10 => 15, 10 operand, + operator
+//substruction -
+// product *
+// division /
+// remainder %
+
+package main
+
+import "fmt"
+
+func main() {
+	// x, y := 15, 10
+
+	// fmt.Printf("%T, %v\n", x, x)
+	// fmt.Printf("%T, %v\n", y, y)
+	// fmt.Printf("%T, %v\n", (x + y), (x + y))
+	// fmt.Printf("%T, %v\n", (x - y), (x - y))
+	// fmt.Printf("%T, %v\n", (x * y), (x * y))
+	// fmt.Printf("%T, %v\n", (x / y), (x / y))
+	// fmt.Printf("%T, %v\n", (x % y), (x % y))
+
+	// z := 5.0 / 2
+
+	// fmt.Printf("%T, %v\n", z, z)
+
+	// Increment ++, Decrement -- POSTFIX
+
+	x := 10
+
+	fmt.Println(x)
+
+	fmt.Println(x + 1)
+
+	x++
+
+	fmt.Println(x)
+}
